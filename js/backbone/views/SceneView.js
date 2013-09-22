7e668f470aed3b79f8da039240a9a614150dea09
gracian.ani@gmail.com <gracian.ani@gmail.com>
2013-09-22 11:13:35 +0800

    fix report bug

--- js/backbone/views/SceneView.js
+++ js/backbone/views/SceneView.js
@@ -64,6 +64,7 @@ var MainView = Backbone.View.extend({
     },
     closeLogin : function() {
         $("#login").addClass("hidden");
+        
         app.LoginFrom = "";
     },
     authorize : function() {
